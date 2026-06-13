04ecec5ef8b8a4ad61c56a5dcb318207e12d9b3c
Justin Kim <justin.joy.9to5@gmail.com>
2026-06-14 08:21:10 +0900

    Add Dovecot LIST publication fixture sink

--- tests/dovecot/fixtures/valid-2.3.21.1/src/lib-storage/mail-storage-private.h
+++ tests/dovecot/fixtures/valid-2.3.21.1/src/lib-storage/mail-storage-private.h
@@ -35,100 +35,125 @@ struct event;
 struct mailbox_vfuncs;
 struct mail_vfuncs;
 
-enum mailbox_feature {
+enum mailbox_feature
+{
   MAILBOX_FEATURE_CONDSTORE = 0x01,
 };
 
-enum mailbox_sync_flags {
+enum mailbox_sync_flags
+{
   MAILBOX_SYNC_FLAG_FULL_READ = 0x01,
   MAILBOX_SYNC_FLAG_FULL_WRITE = 0x02,
   MAILBOX_SYNC_FLAG_FAST = 0x04,
 };
 
-enum mailbox_sync_type {
+enum mailbox_sync_type
+{
   MAILBOX_SYNC_TYPE_EXPUNGE = 0x01,
   MAILBOX_SYNC_TYPE_FLAGS = 0x02,
   MAILBOX_SYNC_TYPE_MODSEQ = 0x04,
 };
 
-struct mailbox_sync_rec {
+enum mailbox_list_child_state
+{
+  MAILBOX_LIST_CHILD_STATE_UNKNOWN = 0,
+  MAILBOX_LIST_CHILD_STATE_HAS_NO_CHILDREN,
+  MAILBOX_LIST_CHILD_STATE_HAS_CHILDREN,
+};
+
+struct mailbox_list_sink_entry
+{
+  const char *name;
+  char hierarchy_delimiter;
+  bool selectable;
+  enum mailbox_list_child_state child_state;
+  const char *special_use;
+};
+
+struct mailbox_sync_rec
+{
   uint32_t seq1;
   uint32_t seq2;
   enum mailbox_sync_type type;
 };
 
-struct mailbox_sync_status {
+struct mailbox_sync_status
+{
   bool sync_delayed_expunges;
 };
 
-struct mailbox_sync_context {
+struct mailbox_sync_context
+{
   struct mailbox *box;
   enum mailbox_sync_flags flags;
   bool open_failed;
 };
 
 const char *mailbox_list_get_storage_name (struct mailbox_list *list,
-                                          const char *vname);
+    const char *vname);
+struct mailbox_list *mailbox_list_sink_alloc (void);
+void mailbox_list_sink_free (struct mailbox_list *list);
+bool mailbox_list_sink_publish_entry (struct mailbox_list *list,
+    const char *name,
+    char hierarchy_delimiter,
+    bool selectable,
+    enum mailbox_list_child_state child_state, const char *special_use);
+unsigned int mailbox_list_sink_get_count (const struct mailbox_list *list);
+const struct mailbox_list_sink_entry *mailbox_list_sink_get_entry (const struct
+    mailbox_list *list, unsigned int index);
 void mail_storage_set_error (struct mail_storage *storage,
-                             enum mail_error error,
-                             const char *string);
+    enum mail_error error, const char *string);
 
-struct mail_storage_vfuncs {
-  struct mail_storage *(*alloc)(void);
-  int (*create)(struct mail_storage *storage,
-                struct mail_namespace *ns,
-                const char **error_r);
-  void (*destroy)(struct mail_storage *storage);
-  void (*add_list)(struct mail_storage *storage,
-                   struct mailbox_list *list);
-  struct mailbox *(*mailbox_alloc)(struct mail_storage *storage,
-                                  struct mailbox_list *list,
-                                  const char *vname,
-                                  enum mailbox_flags flags);
+struct mail_storage_vfuncs
+{
+  struct mail_storage *(*alloc) (void);
+  int (*create) (struct mail_storage * storage,
+      struct mail_namespace * ns, const char **error_r);
+  void (*destroy) (struct mail_storage * storage);
+  void (*add_list) (struct mail_storage * storage, struct mailbox_list * list);
+  struct mailbox *(*mailbox_alloc) (struct mail_storage * storage,
+      struct mailbox_list * list, const char *vname, enum mailbox_flags flags);
 };
 
-struct mailbox_vfuncs {
-  bool (*is_readonly)(struct mailbox *box);
-  int (*enable)(struct mailbox *box, enum mailbox_feature features);
-  int (*open)(struct mailbox *box);
-  int (*get_status)(struct mailbox *box,
-                    enum mailbox_status_items items,
-                    struct mailbox_status *status_r);
-  void (*free)(struct mailbox *box);
-  void (*close)(struct mailbox *box);
-  struct mailbox_sync_context *(*sync_init)(
-      struct mailbox *box,
+struct mailbox_vfuncs
+{
+  bool (*is_readonly) (struct mailbox * box);
+  int (*enable) (struct mailbox * box, enum mailbox_feature features);
+  int (*open) (struct mailbox * box);
+  int (*get_status) (struct mailbox * box,
+      enum mailbox_status_items items, struct mailbox_status * status_r);
+  void (*free) (struct mailbox * box);
+  void (*close) (struct mailbox * box);
+  struct mailbox_sync_context *(*sync_init) (struct mailbox * box,
       enum mailbox_sync_flags flags);
-  bool (*sync_next)(struct mailbox_sync_context *ctx,
-                    struct mailbox_sync_rec *sync_rec_r);
-  int (*sync_deinit)(struct mailbox_sync_context *ctx,
-                     struct mailbox_sync_status *status_r);
-  struct mail_search_context *(*search_init)(
-      struct mailbox_transaction_context *transaction,
-      struct mail_search_args *args,
-      const int *sort,
-      int fields,
-      struct mailbox_header_lookup_ctx *headers);
+  bool (*sync_next) (struct mailbox_sync_context * ctx,
+      struct mailbox_sync_rec * sync_rec_r);
+  int (*sync_deinit) (struct mailbox_sync_context * ctx,
+      struct mailbox_sync_status * status_r);
+  struct mail_search_context *(*search_init) (struct mailbox_transaction_context
+      * transaction, struct mail_search_args * args, const int *sort,
+      int fields, struct mailbox_header_lookup_ctx * headers);
 };
 
-struct mail_vfuncs {
-  int (*get_stream)(struct mail *mail, int get_body,
-                    struct message_size *hdr_size,
-                    struct message_size *body_size,
-                    struct istream **stream);
-  void (*update_flags)(struct mail *mail, int modify_type, int flags);
-  void (*update_keywords)(struct mail *mail, int modify_type,
-                         struct mail_keywords *keywords);
+struct mail_vfuncs
+{
+  int (*get_stream) (struct mail * mail, int get_body,
+      struct message_size * hdr_size,
+      struct message_size * body_size, struct istream ** stream);
+  void (*update_flags) (struct mail * mail, int modify_type, int flags);
+  void (*update_keywords) (struct mail * mail, int modify_type,
+      struct mail_keywords * keywords);
 };
 
-struct virtual_mailbox_vfuncs {
-  void (*get_virtual_uids)(struct mailbox *box,
-                          struct mailbox *backend_mailbox,
-                          const void *backend_uids,
-                          void *virtual_uids_r);
+struct virtual_mailbox_vfuncs
+{
+  void (*get_virtual_uids) (struct mailbox * box,
+      struct mailbox * backend_mailbox,
+      const void *backend_uids, void *virtual_uids_r);
 };
 
-struct mail_storage {
+struct mail_storage
+{
   const char *name;
   unsigned int class_flags;
   struct event *event;
@@ -136,7 +161,8 @@ struct mail_storage {
   struct mail_storage_vfuncs v;
 };
 
-struct mailbox {
+struct mailbox
+{
   const char *name;
   const char *vname;
   struct mail_storage *storage;
@@ -154,7 +180,8 @@ struct mailbox {
   pool_t pool;
 };
 
-struct mail {
+struct mail
+{
   const struct mail_vfuncs *v;
 };
 
--- tests/dovecot/test-dovecot-plugin-mailbox-smoke-stub-registration.c
+++ tests/dovecot/test-dovecot-plugin-mailbox-smoke-stub-registration.c
@@ -8,6 +8,21 @@ struct mail_storage *wyrebox_dovecot_loader_shim_mail_storage_class;
 int wyrebox_dovecot_loader_shim_mail_storage_class_register_calls;
 int wyrebox_dovecot_loader_shim_mail_storage_class_unregister_calls;
 
+struct mailbox_list
+{
+  struct mailbox_list_sink_entry *entries;
+  unsigned int n_entries;
+  unsigned int capacity;
+};
+
+static void
+mailbox_list_sink_entry_clear (struct mailbox_list_sink_entry *entry)
+{
+  free ((char *) entry->name);
+  free ((char *) entry->special_use);
+  memset (entry, 0, sizeof (*entry));
+}
+
 void
 mail_storage_class_register (struct mail_storage *storage_class)
 {
@@ -28,6 +43,87 @@ mailbox_list_get_storage_name (struct mailbox_list *list, const char *vname)
   (void) list;
   (void) vname;
   return NULL;
+}
+
+struct mailbox_list *
+mailbox_list_sink_alloc (void)
+{
+  return calloc (1, sizeof (struct mailbox_list));
+}
+
+void
+mailbox_list_sink_free (struct mailbox_list *list)
+{
+  unsigned int i;
+
+  if (list == NULL)
+    return;
+
+  for (i = 0; i < list->n_entries; i++)
+    mailbox_list_sink_entry_clear (&list->entries[i]);
+
+  free (list->entries);
+  free (list);
+}
+
+bool
+mailbox_list_sink_publish_entry (struct mailbox_list *list,
+    const char *name, char hierarchy_delimiter, bool selectable,
+    enum mailbox_list_child_state child_state, const char *special_use)
+{
+  struct mailbox_list_sink_entry *entry;
+
+  if (list == NULL || name == NULL)
+    return false;
+
+  if (list->n_entries == list->capacity) {
+    unsigned int new_capacity = list->capacity == 0 ? 4 : list->capacity * 2;
+    struct mailbox_list_sink_entry *new_entries;
+
+    new_entries = realloc (list->entries,
+        sizeof (struct mailbox_list_sink_entry) * new_capacity);
+    if (new_entries == NULL)
+      return false;
+
+    list->entries = new_entries;
+    list->capacity = new_capacity;
+  }
+
+  entry = &list->entries[list->n_entries];
+  memset (entry, 0, sizeof (*entry));
+  entry->name = p_strdup (NULL, name);
+  if (entry->name == NULL)
+    return false;
+
+  if (special_use != NULL) {
+    entry->special_use = p_strdup (NULL, special_use);
+    if (entry->special_use == NULL) {
+      mailbox_list_sink_entry_clear (entry);
+      return false;
+    }
+  }
+
+  entry->hierarchy_delimiter = hierarchy_delimiter;
+  entry->selectable = selectable;
+  entry->child_state = child_state;
+  list->n_entries++;
+  return true;
+}
+
+unsigned int
+mailbox_list_sink_get_count (const struct mailbox_list *list)
+{
+  return list != NULL ? list->n_entries : 0;
+}
+
+const struct mailbox_list_sink_entry *
+mailbox_list_sink_get_entry (const struct mailbox_list *list,
+    unsigned int index)
+{
+  if (list == NULL || index >= list->n_entries)
+    return NULL;
+
+  return &list->entries[index];
 }
 
 void
