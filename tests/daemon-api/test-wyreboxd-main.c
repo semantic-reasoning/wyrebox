dad67b4f87136f3f68aecd04872f608cb349ebe1
Justin Kim <justin.joy.9to5@gmail.com>
2026-06-22 21:02:47 +0900

    Add wyreboxd startup path

---
+++ tests/daemon-api/test-wyreboxd-main.c
@@ -0,0 +1,125 @@
+#include <gio/gio.h>
+#include <glib.h>
+#include <glib/gstdio.h>
+
+#include <signal.h>
+#include <sys/stat.h>
+
+static char *
+make_temp_root (void)
+{
+  g_autofree char *root =
+      g_dir_make_tmp ("wyreboxd-main-test-XXXXXX", NULL);
+
+  g_assert_nonnull (root);
+  return g_steal_pointer (&root);
+}
+
+static char *
+write_config (const char *dir, const char *contents)
+{
+  g_autofree char *path = g_build_filename (dir, "wyrebox.conf", NULL);
+  g_autoptr (GError) error = NULL;
+
+  g_assert_true (g_file_set_contents (path, contents, -1, &error));
+  g_assert_no_error (error);
+  g_assert_cmpint (chmod (path, 0600), ==, 0);
+  return g_steal_pointer (&path);
+}
+
+static const char *
+wyreboxd_executable (void)
+{
+  const char *path = g_getenv ("WYREBOXD_EXECUTABLE");
+
+  g_assert_nonnull (path);
+  g_assert_cmpstr (path, !=, "");
+  return path;
+}
+
+static gboolean
+wait_for_socket (const char *socket_path)
+{
+  for (guint i = 0; i < 200; i++) {
+    if (g_file_test (socket_path, G_FILE_TEST_EXISTS))
+      return TRUE;
+    g_usleep (10 * 1000);
+  }
+
+  return FALSE;
+}
+
+static void
+test_wyreboxd_accepts_config_and_starts_socket (void)
+{
+  g_autofree char *root = make_temp_root ();
+  g_autofree char *run_dir = g_build_filename (root, "run", "wyrebox", NULL);
+  g_autofree char *config_path = NULL;
+  g_autofree char *socket_path = NULL;
+  g_autofree char *config_contents = NULL;
+  g_autoptr (GSubprocess) subprocess = NULL;
+  g_autoptr (GError) error = NULL;
+
+  g_assert_cmpint (g_mkdir_with_parents (run_dir, 0750), ==, 0);
+  socket_path = g_build_filename (run_dir, "wyrebox.sock", NULL);
+  config_contents = g_strdup_printf ("[daemon]\n"
+      "socket_path=%s\n", socket_path);
+  config_path = write_config (root, config_contents);
+
+  const char *argv[] = {
+    wyreboxd_executable (),
+    "--config",
+    config_path,
+    NULL
+  };
+
+  subprocess = g_subprocess_newv (argv,
+      (GSubprocessFlags) (G_SUBPROCESS_FLAGS_STDOUT_SILENCE |
+          G_SUBPROCESS_FLAGS_STDERR_PIPE),
+      &error);
+  g_assert_no_error (error);
+  g_assert_nonnull (subprocess);
+
+  g_assert_true (wait_for_socket (socket_path));
+  g_subprocess_send_signal (subprocess, SIGTERM);
+  g_assert_true (g_subprocess_wait (subprocess, NULL, &error));
+  g_assert_no_error (error);
+  g_assert_cmpint (g_subprocess_get_exit_status (subprocess), ==, 0);
+}
+
+static void
+test_wyreboxd_rejects_invalid_config_path (void)
+{
+  g_autoptr (GSubprocess) subprocess = NULL;
+  g_autoptr (GError) error = NULL;
+  const char *argv[] = {
+    wyreboxd_executable (),
+    "--config",
+    "/tmp/wyreboxd-does-not-exist.conf",
+    NULL
+  };
+
+  subprocess = g_subprocess_newv (argv,
+      (GSubprocessFlags) (G_SUBPROCESS_FLAGS_STDOUT_SILENCE |
+          G_SUBPROCESS_FLAGS_STDERR_PIPE),
+      &error);
+  g_assert_no_error (error);
+  g_assert_nonnull (subprocess);
+
+  g_assert_true (g_subprocess_wait (subprocess, NULL, &error));
+  g_assert_no_error (error);
+  g_assert_cmpint (g_subprocess_get_exit_status (subprocess), ==, 78);
+}
+
+int
+main (int argc, char **argv)
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/daemon-api/wyreboxd/starts-with-valid-config",
+      test_wyreboxd_accepts_config_and_starts_socket);
+  g_test_add_func ("/daemon-api/wyreboxd/rejects-invalid-config-path",
+      test_wyreboxd_rejects_invalid_config_path);
+
+  return g_test_run ();
+}
